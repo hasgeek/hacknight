bd58f9646f13cc4d30a54c490454e717f2531e18
Kiran Jonnalagadda <jace@pobox.com>
2012-05-09 13:51:00 +0530

    Coming soon page.

---
+++ instance/settings.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#: Site title
+SITE_TITLE = 'HasGeek Hacknight'
+#: Site id (for network bar)
+SITE_ID = 'hacknight'
+#: Timezone
+TIMEZONE = 'Asia/Calcutta'
+#: LastUser server
+LASTUSER_SERVER = 'https://auth.hasgeek.com/'
+#: Typekit id
+TYPEKIT_CODE = 'qhx6vtv'
+#: Mail settings
+#: MAIL_FAIL_SILENTLY : default True
+#: MAIL_SERVER : default 'localhost'
+#: MAIL_PORT : default 25
+#: MAIL_USE_TLS : default False
+#: MAIL_USE_SSL : default False
+#: MAIL_USERNAME : default None
+#: MAIL_PASSWORD : default None
+#: DEFAULT_MAIL_SENDER : default None
+MAIL_FAIL_SILENTLY = False
+MAIL_SERVER = 'localhost'
+DEFAULT_MAIL_SENDER = ('HasGeek', 'bot@hasgeek.com')
+#: Logging: recipients of error emails
+ADMINS = ['kiran@hasgeek.com']
+#: Log file
+LOGFILE = 'error.log'
