0fdd08c8e1893998dda4426f6fd3e5f04656476c
kracekumar <me@kracekumar.com>
2012-06-03 22:36:20 +0530

    made new, edit page for event

--- hacknight/forms/event.py
+++ hacknight/forms/event.py
@@ -6,11 +6,17 @@ from baseframe.forms import Form, RichTextField
 
 __all__ = ['EventForm']
 
-
+class ValidateEvent(object):
+    @staticmethod
+    def date(self, form, start_datetime='start_datetime', end_datetime='end_datetime'):
+        if start_datetime.data >= end_datetime.data:
+            wtf.ValidationError('End Date must be greater than Start Date')
+        """Need to check if the datetime is past date time, obstacle is Need to have time zone detail, check accordingly.
+        """
 class EventForm(Form):
     title = wtf.TextField("Title", description="Name of the Event", validators=[wtf.Required()])
     description = RichTextField("Description", description="Description of the project")
     #need to datepicker here, for time being while python datetime module is used inside views
     start_datetime = wtf.DateTimeField("StartDateTime", description="Hacknight Start DateTime", validators=[wtf.Required()])
-    end_datetime = wtf.DateTimeField("EndDateTime", description="Hacknight End DateTime", validators=[wtf.Required()])
-    website = wtf.TextField("Main Event Website", validators=[wtf.Optional()])
+    end_datetime = wtf.DateTimeField("EndDateTime", description="Hacknight End DateTime", validators=[wtf.Required(), ValidateEvent.date])
+    website = wtf.TextField("Website",description="Main Event Website", validators=[wtf.Optional()])
--- hacknight/views/profile.py
+++ hacknight/views/profile.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template, g, abort, flash, url_for
-from coaster.views import load_model
+from coaster.views import load_model, load_models
 from baseframe.forms import render_redirect, render_form
 from hacknight import app
 from hacknight.models import db, Profile
@@ -17,7 +17,14 @@ def profile_view(profile):
     return render_template('profile.html', profile=profile)
 
 
-@app.route('/<profile>/new')
+@app.route('/<profile>/<event>', methods=["GET"])
+@load_models(
+  (Profile, {'name': 'profile'}, 'profile'),
+  (Event, {'name': 'event'}, 'event'))
+def event_view(profile, event):
+    return render_template('event.html', event=event)
+
+@app.route('/<profile>/new', methods=['GET', 'POST'])
 @lastuser.requires_login
 def create_event(profile):
     form = EventForm()
@@ -26,13 +33,34 @@ def create_event(profile):
         form.populate_obj(event)
         if not event.name:
             event.make_name()
+            
         db.session.add(event)
         db.session.commit()
         flash(u"You have created new event", "success")
-        return render_redirect(url_for('event_view', event=event.name), code=303)
+        values={'profile': profile, 'event': event.name}
+        return render_redirect(url_for('event_view', **values), code=303)
     return render_form(form=form, title="New Event", submit=u"Create",
         cancel_url=url_for('profile_view', profile=profile), ajax=False)
 
+
+@app.route('/<profile>/<event>/edit', methods=['GET', 'POST'])
+@lastuser.requires_login
+@load_models(
+  (Profile, {'name': 'profile'}, 'profile'),
+  (Event, {'name': 'event', 'profile': 'profile'}, 'event'))
+def event_edit(profile, event):
+    if not lastuser.has_permission('siteadmin') and event.profile.userid not in g.user.user_organization_ids():
+        abort(403)
+    form = EventForm(obj=event)
+    if form.validate_on_submit():
+        form.populate_obj(event)
+        if not event.name:
+            event.make_name()
+        db.session.commit()
+        flash(u"You have edited details for event %s" % event.title, "success")
+        return render_redirect(url_for('event_view', event=event.name, profile=profile.name), code=303)
+    return render_form(form=form, title="Edit Event", submit=u"Save",
+        cancel_url=url_for('event_view', event=event.name), ajax=False)
 
 @app.route('/<profile>/edit', methods=['GET', 'POST'])
 @lastuser.requires_login
