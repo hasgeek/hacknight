e381560be4198e712652976e93a1d92e051507e7
Sajjad Anwar <me@sajjad.in>
2012-06-05 21:43:01 +0530

    Workflow states for participant

--- hacknight/models/participant.py
+++ hacknight/models/participant.py
@@ -4,8 +4,14 @@ from hacknight.models import db, BaseMixin
 from hacknight.models.user import User
 from hacknight.models.event import Event
 
-__all__ = ['Participant']
+__all__ = ['Participant', 'ParticipantStatus']
 
+class ParticipantStatus:
+    PENDING = 0
+    WL = 1
+    CONFIRMED = 2
+    REJECTED = 3
+    WITHDRAWN = 4
 
 class Participant(BaseMixin, db.Model):
     __tablename__ = 'participant'
@@ -13,5 +19,5 @@ class Participant(BaseMixin, db.Model):
     user = db.relationship(User)
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
     event = db.relationship(Event, backref=db.backref('participants', cascade='all, delete-orphan'))
-    status = db.Column(db.Integer, default=0, nullable=False)
+    status = db.Column(db.Integer, default=ParticipantStatus.PENDING, nullable=False)
     mentor = db.Column(db.Boolean, default=False, nullable=False)
