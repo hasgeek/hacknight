7dbe829a81909444a184066fe5884cf47b45766f
kracekumar <kracethekingmaker@gmail.com>
2012-06-06 20:41:12 +0530

    added participant workflow, need to add view details

--- hacknight/views/workflow.py
+++ hacknight/views/workflow.py
@@ -3,7 +3,8 @@
 from flask import g
 from coaster.docflow import DocumentWorkflow, WorkflowState, WorkflowStateGroup
 from hacknight.models.participant import Participant, ParticipantStatus
-from hacknight,views.login import lastuser
+from hacknight.views.login import lastuser
+
 
 class ParticipantWorkFlow(DocumentWorkflow):
     """
@@ -12,7 +13,7 @@ class ParticipantWorkFlow(DocumentWorkflow):
 
     state_attr = 'status'
     pending = WorkflowState(ParticipantStatus.PENDING, title=u'pending', 
-        description=u'State for participants who are interested, 
+        description=u'State for participants who are interested,
                     but waiting for event manager approval')
     waiting_list = WorkflowState(ParticipantStatus.WL, title=u'Waiting List', 
         description=u'State for participants who are interested 
@@ -24,31 +25,63 @@ class ParticipantWorkFlow(DocumentWorkflow):
     withdrawn = WorkflowState(ParticipantStatus.WITHDRAWN, title=u'withdrawn', 
         description=u'State for participants who are uninterested due to 
                       several reason')
-   
-   #States how to become hacknight member
-   #Yes it is very vague name, I need to comeup with very nice name
-   path_to_hacknight = WorkflowStateGroup([pending, waiting_list], title=u'Path 
+    #States how to become hacknight member
+    #Yes it is very vague name, I need to comeup with very nice name
+    path_to_hacknight = WorkflowStateGroup([pending, waiting_list], title=u'Path 
         to hacknight', description=u'If the participant is in any one of the 
         state he/she can become hacknight member')
-
-   #copied from geekup, in hacknight only event manager or site admin can approve
-   def permissions(self):
-       """
-           Permissions available to current user.
-       """
-       base_permissions = super(EventWorkflow,self).permissions()
-       if self.document.user == g.user:
-           base_permissions.append('manager')
-           base_permissions.extend(lastuser.permissions())
-       elif lastuser.has_permission('siteadmin'):
-           base_permissions.append('siteadmin')
-           base_permissions.extend(lastuser.permissions())
-       return base_permissions
+    reject_member = WorkflowStateGroup([pending, waiting_list], 
+    title=u'Path to remove a member from to hacknight',
+    description=u'If the participant is in any one of the 
+        state he/she can be rejected for hacknight') 
+    withdrawn_member = WorkflowStateGroup([accepted, waiting_list, pending], 
+    title=u'Path to withdraw membership',
+    description=u'If the participant is in any one of the 
+        state he/she can withdraw for hacknight')
+    #copied from geekup, in hacknight only event manager can approve
+    def permissions(self):
+        """
+            Permissions available to current user.
+        """
+        base_permissions = super(EventWorkflow,self).permissions()
+        if self.document.user == g.user:
+            base_permissions.append('manager')
+            base_permissions.extend(lastuser.permissions())
+        return base_permissions
     
-   @waiting_list.transition(pending, 'manager', title=u'move to pending list', 
+    @waiting_list.transition(pending, 'manager', title=u'move to pending list', 
        description=u"Move to pending list, waiting for event manager approval",
        view="")
-   def waiting_list_to_pending(self):
-       pass
+    def waiting_list_to_pending(self):
+        pass
+    
+    @pending.transition(confirmed, 'manager', title=u'move to confirmed', 
+       description=u"Move to confirmed from pending list with project owner or 
+       event manager approval", view="")
+    def pending_to_confirm(self):
+        pass
 
+    @confirmed.transition(withdrawn, 'manager', title=u'withdrawn from confimed', 
+       description=u"Withdraw from hacknight", view="")
+    def withdraw_from_hacknight(self):
+        pass
+ 
+    @pending.transition(withdrawn, 'manager', title=u'withdrawnfrom pending', 
+       description=u"Withdraw from hacknight", view="")
+    def withdraw_from_hacknight(self):
+        pass
 
+    @waiting_list.transition(withdrawn, 'manager', title=u'withdrawn from waiting', 
+       description=u"Withdraw from hacknight", view="")
+    def withdraw_from_hacknight(self):
+        pass
+
+    @waiting_list.transition(rejected, 'manager', title=u'rejected state from waiting list', 
+       description=u"Rejected from hacknight", view="")
+    def rejected_from_hacknight(self):
+        pass
+
+    @pending.transition(rejected, 'manager', title=u'rejected from pending', 
+       description=u"Withdraw from hacknight", view="")
+    def withdraw_from_hacknight(self):
+        pass
