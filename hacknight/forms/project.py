bf0b77008d4a2ffa6a31aee80dc51f958c94818b
Sajjad Anwar <me@sajjad.in>
2012-06-14 18:08:58 +0530

    Fixes #26, radio buttons now have a default value

--- hacknight/forms/project.py
+++ hacknight/forms/project.py
@@ -10,7 +10,7 @@ class ProjectForm(Form):
     title = wtf.TextField('Title', description="Title of the project", validators=[wtf.Required('A title is required')])
 
     description = RichTextField(u"Description", description="Describe your project with supporting links to datasets or other preperatory material. Also say why you want to work on it.")
-    status = wtf.RadioField("Are you hacking?", coerce=int,
+    status = wtf.RadioField("Are you hacking?",default=0, coerce=int,
         choices=[(0, u"I will be working on this project"),
                  (1, u"I’m proposing to invite someone to take it up")])
 
