e0445c0988f0d4d21dbe1118199016499ce4257e
kracekumar <kracethekingmaker@gmail.com>
2012-06-16 19:30:17 +0530

    Fixed Issues #30

--- hacknight/forms/event.py
+++ hacknight/forms/event.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import flask.ext.wtf as wtf
-from baseframe.forms import Form, RichTextField, DateTimeInput
+from baseframe.forms import Form, RichTextField, DateTimeField
 
 __all__ = ['EventForm']
 
@@ -9,8 +9,8 @@ class EventForm(Form):
     title = wtf.TextField("Title", description="Name of the Event", validators=[wtf.Required(), wtf.NoneOf(values=["New"])])
     description = RichTextField("Description", description="Description of the project")
     #need to datepicker here, for time being while python datetime module is used inside views
-    start_datetime = wtf.DateTimeField("Start Date and Time", widget=DateTimeInput(), description="Hacknight Start DateTime", validators=[wtf.Required()])
-    end_datetime = wtf.DateTimeField("End Date and Time", widget=DateTimeInput(), description="Hacknight End DateTime", validators=[wtf.Required()])
+    start_datetime = DateTimeField("Start Date and Time", description="Hacknight Start DateTime", validators=[wtf.Required()])
+    end_datetime = DateTimeField("End Date and Time", description="Hacknight End DateTime", validators=[wtf.Required()])
     ticket_price = wtf.TextField("Ticket Price", description="Entry fee, if any, to be paid at the venue.")
     total_participants = wtf.IntegerField("Total Participants", description="Total Participants for Hacknight. E.g: 50", default=50, validators=[wtf.Required()])
     website = wtf.TextField("Website",description="Related Website (Optional)", validators=[wtf.Optional()])
