844424ca89be98ef466ce32a8c6092f946521bbe
kracekumar <kracethekingmaker@gmail.com>
2012-06-28 22:36:23 +0530

    Replaced Funcname for fulldate template filter

--- hacknight/views/index.py
+++ hacknight/views/index.py
@@ -8,21 +8,22 @@ from hacknight.models.event import Profile
 
 tz = timezone(app.config['TIMEZONE'])
 
+
 @app.route('/')
 def index():
-	events = Event.query.filter_by(status=0).all()
-	if g.user:
-		profile = Profile.query.filter_by(userid=g.user.userid).first()
-		return render_template('index.html', events=events, profile=profile)
-
-	else:
-		return render_template('index.html', events=events)
+    events = Event.query.filter_by(status=0).all()
+    if g.user:
+        profile = Profile.query.filter_by(userid=g.user.userid).first()
+        return render_template('index.html', events=events, profile=profile)
+    else:
+        return render_template('index.html', events=events)
 
 
 @app.template_filter('shortdate')
 def shortdate(date):
     return utc.localize(date).astimezone(tz).strftime("%B %d, %Y")
 
+
 @app.template_filter('fulldate')
-def shortdate(date):
+def fulldate(date):
     return utc.localize(date).astimezone(tz).strftime("%a, %b %e %H: %M %P")
