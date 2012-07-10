c8fae6ddeb1a34877204b143255b226b1d2e96ed
Sajjad Anwar <me@sajjad.in>
2012-07-10 19:57:53 +0530

    Optional validator for sponsor website

--- hacknight/forms/sponsor.py
+++ hacknight/forms/sponsor.py
@@ -7,12 +7,12 @@ __all__ = ['SponsorForm']
 
 
 class SponsorForm(Form):
-    title = wtf.TextField("Title", description="Title of the project", 
-    	validators=[wtf.Required("A title is required")])
+    title = wtf.TextField("Title", description="Title of the project",
+        validators=[wtf.Required("A title is required")])
     description = RichTextField(u"Description",
         description="Detailed description of your project", width="50%",
         content_css="/static/css/editor.css")
-    website = wtf.html5.URLField("Home Page", 
-    	description="URL to the home page")
+    website = wtf.html5.URLField("Home Page",
+        description="URL to the home page", validators=[wtf.Optional()])
     image_url = wtf.html5.URLField("Image URL", description="URL to the image",
-    	validators=[wtf.Required("An image is required.")])
+        validators=[wtf.Required("An image is required.")])
