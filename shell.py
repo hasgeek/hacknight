3dc3460911f9302a33970799d1fda20aec11dfa7
kracekumar <me@kracekumar.com>
2013-01-16 13:48:40 +0530

    Added shell.py

---
+++ shell.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+import os
+import readline
+from pprint import pprint
+
+from flask import *
+from hacknight import *
+
+os.environ['PYTHONINSPECT'] = 'True'
