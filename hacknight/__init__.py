fb2e459dbc802e856ecf03690f8bfc667e740654
kracekumar <me@kracekumar.com>
2013-05-24 16:57:07 +0530

    Added _version.py, new baseframe migration

--- hacknight/__init__.py
+++ hacknight/__init__.py
@@ -8,11 +8,13 @@ from flask.ext.lastuser import Lastuser
 from flask.ext.lastuser.sqlalchemy import UserManager
 from flask.ext.mail import Mail
 from flask.ext.assets import Environment, Bundle
-from baseframe import baseframe, baseframe_js, baseframe_css
+from baseframe import baseframe, assets, Version#, baseframe_js, baseframe_css
 import coaster.app
+from ._version import __version__
 
 # First, make an app and config it
 
+version = Version(__version__)
 app = Flask(__name__, instance_relative_config=True)
 lastuser = Lastuser()
 mail = Mail()
@@ -26,21 +28,15 @@ import hacknight.views
 # Third, setup baseframe and assets
 
 app.register_blueprint(baseframe)
-
-assets = Environment(app)
-js = Bundle(baseframe_js,
-    'js/leaflet/leaflet.js',
-    'js/scripts.js')
-
-css = Bundle(baseframe_css,
-    'js/leaflet/leaflet.css',
-    'css/app.css')
-assets.register('js_all', js)
-assets.register('css_all', css)
+assets['leaflet.css'][version] = 'js/leaflet/leaflet.css'
+assets['leaflet.js'][version] = 'js/leaflet/leaflet.js'
+assets['hacknight.css'][version] = 'css/app.css'
+assets['hacknight.js'][version] = 'js/scripts.js'
 
 
 def init_for(env):
     coaster.app.init_app(app, env)
+    baseframe.init_app(app, requires=['baseframe', 'hacknight', 'leaflet'])
     lastuser.init_app(app)
     lastuser.init_usermanager(UserManager(hacknight.models.db, hacknight.models.User))
     mail.init_app(app)
