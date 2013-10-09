a52ab3cf15dd49dd62cf77f90a3f8068a2960b94
kracekumar <me@kracekumar.com>
2013-10-10 00:47:14 +0530

    moved line back to original pos

--- hacknight/views/event.py
+++ hacknight/views/event.py
@@ -229,9 +229,9 @@ def event_apply(profile, event):
             form.populate_obj(participant)
             participant.save_defaults()
             participant.status = PARTICIPANT_STATUS.PENDING if event.maximum_participants > total_participants else PARTICIPANT_STATUS.WL        
-            flash(u"Your request to participate has been recorded; you will be notified by the event manager", "success")
             db.session.add(participant)
             db.session.commit()
+            flash(u"Your request to participate has been recorded; you will be notified by the event manager", "success")
         else:
             return render_form(form=form, message=Markup(event.apply_instructions) if event.apply_instructions else "",
                 title="Participant Details", submit=u"Participate",
