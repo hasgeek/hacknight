598b617954480bf2ff3d3dfeb87dce1fdee01663
Bibhas <iambibhas@gmail.com>
2017-10-14 01:31:27 +0530

    added test settings

---
+++ instance/testing.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#: Debug mode (put this value in development.py only, never in settings.py)
+DEBUG = True
+#: Site title
+SITE_TITLE = 'HasGeek App'
+#: Site id (for network bar)
+SITE_ID = ''
+#: Database backend
+SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+#: Secret key
+SECRET_KEY = 'make this something random'
+#: Timezone
+TIMEZONE = 'Asia/Calcutta'
+#: LastUser server
+LASTUSER_SERVER = 'https://auth.hasgeek.com/'
+#: LastUser client id
+LASTUSER_CLIENT_ID = ''
+#: LastUser client secret
+LASTUSER_CLIENT_SECRET = ''
+#: Mail settings
+#: MAIL_FAIL_SILENTLY : default True
+#: MAIL_SERVER : default 'localhost'
+#: MAIL_PORT : default 25
+#: MAIL_USE_TLS : default False
+#: MAIL_USE_SSL : default False
+#: MAIL_USERNAME : default None
+#: MAIL_PASSWORD : default None
+#: DEFAULT_MAIL_SENDER : default None
+MAIL_FAIL_SILENTLY = False
+MAIL_SERVER = 'localhost'
+DEFAULT_MAIL_SENDER = ('HasGeek', 'test@example.com')
+#: Logging: recipients of error emails
+ADMINS = []
+#: Log file
+LOGFILE = 'error.log'
